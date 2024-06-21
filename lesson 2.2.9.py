b3286c3d64d54bf073a7ae1b475223d85cde2bb5
RedC0mrade <e2e4e2e4@mail.ru>
2024-06-21 17:10:44 +0200

    12312

---
+++ lesson 2.2.9.py
@@ -0,0 +1,12 @@
+from re import split
+
+with open(r'C:\Downloads\files.txt', 'r', encoding='utf-8') as file:
+    result = {}
+    for i in file:
+        n = split(r'\.|\s', i.strip())
+        result.setdefault(n[1], []).append(i.strip())
+
+for i, k in result.items():
+    print(sorted(k))
+#result = sorted(result, key=lambda x: result[x])
+#print(result)
