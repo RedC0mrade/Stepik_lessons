d30fd3d025fd2f92a78b0029fe9f28bf0e4ed6cc
RedC0mrade <e2e4e2e4@mail.ru>
2024-07-11 12:11:36 +0200

    12312

---
+++ lesson 2.2.7.py
@@ -0,0 +1,22 @@
+glas = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
+word = input()
+words = [input() for i in range(int(input()))]
+n=[]
+
+for i in word:
+    if i in glas:
+        word = word.replace(i, '*', 1)
+    else:
+        word = word.replace(i, '_', 1)
+word = word.rstrip('_')
+for j in words:
+    n1 = j
+    for i in j:
+        if i in glas:
+            j = j.replace(i, '*', 1)
+        else:
+            j = j.replace(i, '_', 1)
+    j = j.rstrip('_')
+    if word == j:
+        n.append(n1)
+print(*n, sep='\n')
